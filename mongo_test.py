a22145c9e2fc366996cd352494e08a5d329a47a3
vishnu <vishnudasari1905@gmail.com>
2026-06-28 11:43:02 +0000

    cached rm

--- mongo_test.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-# Connect to Windows MongoDB from WSL
-client = MongoClient("mongodb://host.docker.internal:27017/")
-
-# Test connection
-print(client.list_database_names())
-
